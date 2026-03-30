89d54514314baf73ea022829a5ca97009659d6a5
David Antúnez Pérez <91873698+dantudev@users.noreply.github.com>
2026-03-30 19:51:56 +0200

    feat: dependency vulnerabilities and documentation update

--- src/global.constants.ts
+++ src/global.constants.ts
@@ -1,5 +1,5 @@
 // URL DIRECTIONS
 export const EMAIL_URL = 'mailto:antunezdavid2003@gmail.com'
 export const GITHUB_URL =
-  'https://github.com/DavidAntunezPerez/schulte-table-web-app'
+  'https://github.com/dantudev/schulte-table-web-app'
 export const LINKEDIN_URL = 'https://linkedin.com/in/davidantunezperez/'
